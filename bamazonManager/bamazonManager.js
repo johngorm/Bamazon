0b47868f57d9c94e7f04bfa40ded30b4c0e8922e
John Gorman <johngorm@usc.edu>
2017-04-24 21:51:56 -0500

    Show all items in databasefor option 1

--- bamazonManager/bamazonManager.js
+++ bamazonManager/bamazonManager.js
@@ -2,7 +2,31 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 require('console.table');
 
+const connection = mysql.createConnection({
+	host : 'localhost',
+	port: 3306,
 
+	user: 'root',
+
+	password: '',
+	database: 'Bamazon'
+});
+
+function displayDBProducts(){
+	connection.query('SELECT * FROM products', (err, res) => {
+		if(err){
+			throw err;
+		}
+		let recordArray = [];
+		for(let record in res){
+			recordArray.push(res[record]);
+		}
+		console.log('\n');
+		console.table(recordArray);
+		displayMenuOptions();
+	});
+
+}
 
 
 
@@ -12,12 +36,25 @@ function displayMenuOptions(){
 		name: 'manager_option',
 		type: 'rawlist',
 		message: 'Select your desired action.',
-		choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
+		choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'QUIT']
 	}
 	]).then(function(response) {
 		var choice = response.manager_option;
 		if(choice === 'View Products for Sale'){
-			console.log('x');
+			displayDBProducts();
+		}
+		else if(choice === 'View Low Inventory'){
+			displayLowInventory();
+		}
+		else if(choice === 'Add to Inventory'){
+			addToInventory();
+		}
+		else if(choice === 'Add New Product'){
+			addNewProduct();
+		}
+		else{
+			connection.end();
+			process.exit();
 		}
 	})
 };
